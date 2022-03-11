0641a06643d86fa143c56c370cf7cdf0dc80c16f
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-03-11 18:28:57 +0530

    updates done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,12 @@ const jwt = require('jsonwebtoken')
 
 const SECRET_KEY = "hitheremynameisvishalsachananiamfromkanpur"
 
-const createJwtToken = (userName, contact, email) => {
-    return jwt.sign({ userName, contact, email }, SECRET_KEY, { expiresIn: "2d" });
+// const createJwtToken = (userName, contact, email) => {
+//     return jwt.sign({ userName, contact, email }, SECRET_KEY, { expiresIn: '1d' });
+// }
+
+const newJwtToken = (id) => {
+    return jwt.sign({ id }, SECRET_KEY, { expiresIn: '1d' })
 }
 
 const registerUser = async (req, res, next) => {
@@ -18,17 +22,13 @@ const registerUser = async (req, res, next) => {
             message: "This Email is already Registered"
         })
     }
-    const token = createJwtToken({ userName, contact, email })
-    const createdUser = await User.create({ userName, contact, email, password: hashedpass, token });
+    // const token = createJwtToken({ userName, contact, email })
+    const createdUser = await User.create({ userName, contact, email, password: hashedpass });
     if (createdUser) {
-        const { userName, contact, email, token } = createdUser
+        // const { userName, contact, email, token } = createdUser
         //console.log(createdUser)
         res.json({
             message: 'Registered',
-            userName,
-            contact,
-            email,
-            token
         })
     }
     else {
@@ -38,49 +38,84 @@ const registerUser = async (req, res, next) => {
     }
 }
 
+// const loginUser = async (req, res, next) => {
+//     const { email, token, password } = req.body;
+//     const userExists = await User.findOne({ email })
+//     if (userExists) {
+//         const correctPass = await bcrypt.compare(password, userExists.password)
+//         //console.log(correctPass)
+//         if (correctPass) {
+//             const { userName, contact, email } = userExists
+//             jwt.verify(token, SECRET_KEY, (err, decoded) => {
+//                 if (err) {
+//                     const newToken = createJwtToken({ userName, contact, email })
+//                     const updatedUser = User.updateOne({ email: email }, { $set: { token: newToken } })
+//                     //console.log(updatedUser.acknowledged)
+//                     if (updatedUser.acknowledged) {
+//                         sessionStorage.setItem('token', newToken)
+//                         return res.status(201).json({
+//                             message: 'Token has Expired new token Generated',
+//                             isLogin: true
+//                         })
+//                     }
+//                 }
+//                 res.status(201).json({
+//                     decoded: decoded,
+//                     isLogin: true
+//                 })
+//             })
+//         }
+//         else {
+//             res.status(201).json({
+//                 message: 'Password Incorrect',
+//                 isLogin: false
+//             })
+//         }
+//     }
+//     else {
+//         res.status(201).json({
+//             message: 'This User is not Registered',
+//             isLogin: false
+//         })
+//     }
+// }
+
 const loginUser = async (req, res, next) => {
-    const { email, token, password } = req.body;
+    const { email, password } = req.body;
     const userExists = await User.findOne({ email })
-    if (userExists) {
-        const correctPass = await bcrypt.compare(password, userExists.password)
-        //console.log(correctPass)
-        if (correctPass) {
-            const { userName, contact, email } = userExists
-            jwt.verify(token, SECRET_KEY, (err, decoded) => {
-                if (err) {
-                    const newToken = createJwtToken({ userName, contact, email })
-                    const updatedUser = User.updateOne({ email: email }, { $set: { token: newToken } })
-                    //console.log(updatedUser.acknowledged)
-                    if (updatedUser.acknowledged) {
-                        sessionStorage.setItem('token', newToken)
-                        return res.status(201).json({
-                            message: 'Token has Expired new token Generated',
-                            isLogin: true
-                        })
-                    }
-                }
-                res.status(201).json({
-                    decoded: decoded,
-                    isLogin: true
-                })
-            })
-        }
-        else {
-            res.status(201).json({
-                message: 'Password Incorrect',
-                isLogin: false
-            })
-        }
+    if (!userExists) {
+        return res.status(201).json({
+            message: 'This User is not Registered',
+            // token: null
+        })
+    }
+    if (await bcrypt.compare(password, userExists.password)) {
+        res.status(201).json({
+            message: 'Login Sucessfull',
+            token: newJwtToken(userExists._id),
+            user: userExists
+        })
     }
     else {
         res.status(201).json({
-            message: 'This User is not Registered',
-            isLogin: false
+            message: 'Password Incorrect',
+            // token: null
         })
     }
 }
 
+const tokenValidate = async (req, res, next) => {
+    const token = req.body.token
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (decoded) {
+            return res.status(201).json({
+                token: newJwtToken(decoded._id)
+            })
+        }
+    })
+}
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    tokenValidate
 }
