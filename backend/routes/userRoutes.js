592bcc2cb0af25b91b8ed45dcb6416d61a22cdcb
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-03-11 18:29:01 +0530

    updates done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController')
 
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
+router.post('/verify', userController.tokenValidate)
 
 module.exports = router;
