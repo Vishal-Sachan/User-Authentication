135083afa12c983943d73f2c573f6c98d24be48a
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-03-16 12:50:23 +0530

    personal detail component ok

--- backend/controllers/personal-detail-Controller.js
+++ backend/controllers/personal-detail-Controller.js
@@ -1,6 +1,21 @@
 const Details = require('../models/personal-details.model')
 
-const ViewDetail = async (req, res, next) => { }
+const ViewDetail = async (req, res, next) => {
+    try {
+        console.log(req.body)
+        const { email } = req.body
+        const response = await Details.findOne({ email })
+        console.log(response)
+        res.json({
+            data: response
+        })
+    } catch (err) {
+        // console.log(err)
+        res.json({
+            message: "no personal data available"
+        })
+    }
+}
 
 const AddDetail = async (req, res, next) => { }
 
